d308956e18f0241a59b681849bdbc3f1a15fa468
yogi365 <sharma.yogendra@yahoo.com>
2024-07-31 16:01:51 +0530

    Second commit

--- 05 JS Functions 29 July/function.js
+++ 05 JS Functions 29 July/function.js
@@ -53,11 +53,103 @@
 
 // https://github.com/yogi365/fee-g
 
-var sum;
+// var sum;
 
 
-function sum(){
-    console.log("Hello")    
-}
+// function sum(){
+//     console.log("Hello")    
+// }
 
-console.log(sum);
+// console.log(sum);
+
+// var sum = function(a,b){
+//     return a+b;
+// }
+
+
+// var sum = (a,b)=>{
+//     return a;
+// }
+
+// console.log(sum(2,6));
+
+
+// var sum = a=>{
+//     b=0;
+//     return a;
+// }
+
+// console.log(sum(2,6));
+
+// var sum = (a,b)=>  a+b;
+
+
+// console.log(sum(2,6));
+
+// var sum = a=>  console.log(a*a);
+// sum(6)
+
+// console.log(sum(6));
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function difference(a,b){
+//     return b-a;
+// }
+
+// function multiply(a,b){
+//     return a*b;
+// }
+
+// function divide(a,b){
+//     return a/b;
+// }
+//callback
+// function arithmeticOperationsOfSquaredNumber(number1,number2,operations){
+//     number1 = number1*number1;
+//     number2 = number2 * number2;
+//     return operations(number1,number2);
+// }
+// var a = (a,b)=>a+b;
+// console.log(arithmeticOperationsOfSquaredNumber(8,9,(a,b)=>a+b))
+
+// function arithmeticOperations(number1, number2, operations) {
+//     if (operations == '+')
+//         return sum(number1, number2);
+//     else if (operations == '-')
+//         return difference(number1, number2);
+//     else if (operations == '*')
+//         return multiply(number1, number2);
+//     else if (operations == '/')
+//         return divide(number1, number2);
+//     else
+//         return "invalid operations";
+// }
+
+// console.log(arithmeticOperations(2,6,'='));
+
+//iife - instantly invoking function execution
+
+// (
+//     function abc(){
+//         console.log("Hello");
+//     }
+// )()
+
+
+
+// (
+//     function(name){
+//         var a = 10;
+//         var b = 20;
+//         for (let i = 0; i < a; i++) {
+    
+// }
+//     }
+// )("Abc")
+
+
+
+
