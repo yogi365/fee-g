7c77c82acb71d81699f978a802c4482793e926f0
yogi365 <sharma.yogendra@yahoo.com>
2024-08-12 10:11:14 +0530

    Iterators

---
+++ 08 JS Array 9 August/array.js
@@ -0,0 +1,32 @@
+// var arr = new Array();
+// var arr = [];
+// var arr2 =0;
+// arr2 = [1,2,3,4,5]
+
+// var arr = [1,2,3,4,5,true, undefined, "Hello",[6,7,8,9,10,[11,12,13]],{name:"Gurwinder",age:18}];
+// var innnerArr = arr[8]
+// arr[8] = "12"
+// console.log(arr)
+// console.log(arr.length)
+// arr[20]="Last Value";
+// console.log(arr)
+
+// push pop shift and unshift
+
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+// arr.pop();
+// arr.push(11)
+// arr.shift();
+// arr.unshift(12)
+// console.log(arr)
+
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+
+// arr.splice(3,4,1,2,3,4,5,8);
+// console.log(arr);
+
+
+var arr = [1,2,3,4,5,6,7,8,9,10]
+
+var str = arr.toString();
+console.log(typeof str);
---
+++ 09 JS Array Iterators 12 August/iterators.js
@@ -0,0 +1,42 @@
+// Map, forEach, filter, find
+
+// var arr = [1,2,3,4,5,6,7];
+
+// var newArr = arr.map((element, index,array)=>{
+//     return element * element;
+// })
+// console.log(newArr);
+
+// var filterArr = arr.filter((element,index,array)=>{
+//     return element%2==0;
+// })
+
+// function filter(callback){
+//     var newArr = [];
+//     for(let i=0;i<arr.length;i++){
+//         if(callback(element,i,arr)){
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     return newArr;
+// }
+
+// var value = arr.find((element,index,array)=>element%2==0);
+
+// console.log(value);
+
+// arr.forEach((element,index,array)=>{
+//     console.log(element,index,array)
+// })
+
+// function forEach(callback){
+//     for(let i=0;i<arr.length;i++){
+//         callback(arr[i],i,arr);
+//     }
+// }
+
+// var newArr = [8,5,2,1,9,3,5,2,4,22,3];
+
+// newArr.sort((a,b)=>b-a);
+
+// console.log(newArr)
